a469a6ba52078fd79bea137b87ebe7664d148e69
evgen sikora <arnika200@gmail.com>
2022-10-03 10:52:21 +0300

    some changes in code

--- circular-buffer.js
+++ circular-buffer.js
@@ -1,51 +1,55 @@
 class CircularBuffer {
+    buffer;
+    start;
+    end;
+    size;
+
     constructor(length) {
-        this.buffer = new Array(length).fill(undefined); //[].length
+        this.buffer = new Array(length);
         this.start = 0;
         this.end = 0;
         this.size = 0;
-        this.lenght = length;
+        this.length = length;
     };
 
     write(item) {
         if (this.isFull()) throw new BufferFullError();
         this.buffer[this.end] = item;
-        this.end = (this.end + 1) % this.lenght;
+        this.end = (this.end + 1) % this.length;
         this.size++;
     };
 
     read() {
         if (this.isEmpty()) throw new BufferEmptyError();
         let item = this.buffer[this.start];
-        this.buffer[this.start] = undefined;
-        this.start = (this.start + 1) % this.lenght;
+        this.start = (this.start + 1) % this.length;
         this.size--;
         return item;
     };
 
     forceWrite(item) {
         try {
-            this.write(item)
+            this.write(item);
         } catch (err) {
-            if (!typeof err === 'BufferFullError') { throw err}
-            this.read()
-            this.write(item)
+            if (!typeof err === 'BufferFullError') {
+                throw err;
+            }
+            this.read();
+            this.write(item);
         }
-    }
+    };
 
     clear() {
-        [this.buffer, this.start, this.end, this.size] = [new Array(this.lenght), 0, 0, 0];
+        [this.buffer, this.start, this.end, this.size] = [new Array(this.length), 0, 0, 0];
     }
-
 
     isEmpty() {
         return this.size === 0;
     }
 
     isFull() {
-        return this.size === this.lenght;
+        return this.size === this.length;
     }
-
 }
 
 export default CircularBuffer;
